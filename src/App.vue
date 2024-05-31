c1abe98fa7d1be0aa78b4cf39528a3f8cd6c49f0
marius <marius.horghidan1@gmail.com>
2024-05-31 20:03:23 +0300

    SIX-1001: commit 1
    SIX-1001: commit 2

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@ import CommentsForm from './components/CommentsForm.vue'
 console.log('MAIN CHANGE')
 console.log('commit 1')
 console.log('commit 2')
+console.log('commit 3')
+console.log('commit 4')
 </script>
 
 <template>
